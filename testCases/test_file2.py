f1af397fb0041cb4c50d920950182e3452beed2d
Megha-Ravsaheb-Yadav <meghayadav3446@gmail.com>
2023-05-23 11:22:14 +0530

    My First commit

--- testCases/test_file2.py
+++ testCases/test_file2.py
@@ -5,10 +5,10 @@ class Test_002:
 
     @pytest.mark.group1
     def test_sum_004(self):
-        a = 3
+        a = 4
         b = 4
         sum = a + b
-        if sum == 7:
+        if sum == 8:
             assert True
         else:
             assert False
